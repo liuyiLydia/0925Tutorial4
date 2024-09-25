9ef7beea91bf3be4c59fd66ddcb1f9bf8a560e61
liuyiLydia <1160500334@qq.com>
2024-09-25 20:40:34 +0800

    add changes again

--- src/test/java/org/example/TestSubtraction.java
+++ src/test/java/org/example/TestSubtraction.java
@@ -1,6 +1,14 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+
 public class TestSubtraction {
 
+    Calc c = new Calc();
     @Test
+    void testSubtraction()
+    {assertEquals(2, c.subtract(4,2));}
 
-    void testSubtraction(){assertEquals(2, c.subtract(4,2));}
 }
